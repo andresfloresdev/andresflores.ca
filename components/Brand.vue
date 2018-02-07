90e939fdec2a75027c445ab6324b4335468695c2
Andres Flores <andres@2associes.com>
2018-02-07 12:09:11 -0500

    Remove useless ID

--- components/Brand.vue
+++ components/Brand.vue
@@ -1,7 +1,7 @@
 <template>
   <svg width="100%" height="100%" viewBox="0 0 808 591" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:1.41421;">
     <g transform="matrix(1,0,0,1,-420.918,-1750.4)">
-      <g id="Artboard1" transform="matrix(1.08657,0,0,0.886163,-237.425,199.261)">
+      <g transform="matrix(1.08657,0,0,0.886163,-237.425,199.261)">
         <g clip-path="url(#_clip1)">
           <g transform="matrix(1.92686,0,0,2.36261,-3782.89,534.346)">
             <g transform="matrix(1,0,0,1,1577.23,-395.304)">
