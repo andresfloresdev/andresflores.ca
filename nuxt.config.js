45a389b222e5b9a638d71fce0021cfc44206f91a
Andres Flores <andres@2associes.com>
2018-02-07 12:26:08 -0500

    Add lang attribute to html tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,6 +21,9 @@ module.exports = {
     '~/plugins/vue-scrollto'
   ],
   head: {
+    htmlAttrs: {
+      lang: 'en-CA',
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
