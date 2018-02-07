18ba36106143c922fffc3929bdfd4e255207092f
Andres Flores <andres@2associes.com>
2018-02-07 15:28:55 -0500

    Add eslint packages and configs

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,6 +9,16 @@ module.exports = {
     color: '#427fed'
   },
   build: {
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    },
     postcss: [
       require('autoprefixer')(),
     ]
