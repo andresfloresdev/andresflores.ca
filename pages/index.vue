7ea9085bdc55857906c4e116f12325a1c87bdbb5
Andres Flores <andres@2associes.com>
2018-02-08 18:08:41 -0500

    Add meta cards for Twitter and Facebook

--- pages/index.vue
+++ pages/index.vue
@@ -22,6 +22,27 @@ export default {
     Clients,
     Work,
     Skills
+  },
+  head () {
+    return {
+      title: 'Andres Flores, Web Developer',
+      meta: [
+        // Twitter Card
+        { name:'twitter:card', content:'summary'},
+        { name:'twitter:creator', content:'@andresfloresdev'},
+        { name:'twitter:url', content:'https://andresflores.ca/'},
+        { name:'twitter:title', content:'Andres Flores, Web Developer'},
+        { name:'twitter:description', content:'Web Developer based in Montréal, Canada.'},
+        { name:'twitter:image', content:'https://andresflores.ca/images/dark-bg-1200x.png'},
+        // Facebook Open Graph
+        { property: 'og:url', content: 'https://andresflores.ca/'},
+        { property: 'og:type', content: 'website'},
+        { property: 'og:title', content: 'Andres Flores, Web Developer'},
+        { property: 'og:image', content: 'https://andresflores.ca/images/dark-bg-1200x.png'},
+        { property: 'og:description', content: 'Web Developer based in Montréal, Canada.'},
+        { property: 'og:locale', content: 'en-CA'}
+      ]
+    }
   }
 }
 </script>
