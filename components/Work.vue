c54cf7947580d25ea55e2d3e6a29583dc449cd4d
Andres Flores <andres@2associes.com>
2018-02-08 19:49:27 -0500

    Clean Work component code

--- components/Work.vue
+++ components/Work.vue
@@ -12,7 +12,9 @@
                 {{ single.title }}
               </a>
             </h3>
-            <p class="work-single-text" v-html="single.text"></p>
+            <p class="work-single-text">
+              {{ single.text }}
+            </p>
           </div>
           <figure class="work-single-figure">
             <img
@@ -22,7 +24,7 @@
         </article>
       </transition-group>
       <div class="work-count">
-        <span class="work-count-current">0{{ current }}</span>/<span class="work-count-total">0{{ count }}</span>
+        <span class="work-count-current">{{ doubleDigits(current) }}</span>/<span class="work-count-total">{{ doubleDigits(count) }}</span>
       </div>
       <ul class="work-actions">
         <li class="work-action-prev">
@@ -52,37 +54,37 @@ export default {
         {
           title: 'David\'s Tea',
           link: 'https://www.davidstea.com/',
-          text: 'As a Junior Web Developer for <a href="https://2associes.com">2 Associés</a>, I integrated the markup and styles of a couple of pages of the client\'s new website. I received multiple Photoshop and Illustrator files and had to translate the mobile, tablet and desktop views into semantic, reusable code. It was an interesting experience to work with Commerce Cloud.',
+          text: 'As a Junior Web Developer for 2 Associés, I integrated the markup and styles of a couple of pages of the client\'s new website. I received multiple Photoshop and Illustrator files and had to translate the mobile, tablet and desktop views into semantic, reusable code. It was an interesting experience to work with Commerce Cloud.',
           imageURL: 'davids-tea'
         },
         {
           title: 'Parler SST',
           link: 'https://parlersst.com/',
-          text: 'As a Junior Web Developer for <a href="https://2associes.com">2 Associés</a>, I integrated the markup, styles and Javascript functionality of this page. Browser compatibility was a top priority, so it allowed me to hone my skills with the basics that I learned in college/university. I usually don\'t go as far back as IE10-compatible code with any of my projects, but it was a fun, learning experience.',
+          text: 'As a Junior Web Developer for 2 Associés, I integrated the markup, styles and Javascript functionality of this page. Browser compatibility was a top priority, so it allowed me to hone my skills with the basics that I learned in college/university. I usually don\'t go as far back as IE10-compatible code with any of my projects, but it was a fun, learning experience.',
           imageURL: 'parler-sst'
         },
         {
           title: 'DuProprio',
           link: 'https://duproprio.com/en',
-          text: 'As a Junior Web Developer for <a href="https://2associes.com">2 Associés</a>, I integrated banner ads with Google Web Designer while learning the software. I was able to manually extract assets from Photoshop files, optimize, integrate and animate them according to the client\'s frame guidelines. I had a lot of fun working with the software and finding new ways to improve the workflow.',
+          text: 'As a Junior Web Developer for 2 Associés, I integrated banner ads with Google Web Designer while learning the software. I was able to manually extract assets from Photoshop files, optimize, integrate and animate them according to the client\'s frame guidelines. I had a lot of fun working with the software and finding new ways to improve the workflow.',
           imageURL: 'duproprio'
         },
         {
           title: 'Laura Canada',
           link: 'https://www.laura.ca/',
-          text: 'As a Junior Web Developer for <a href="https://2associes.com">2 Associés</a>, I integrated a few landing pages for Laura Canada\'s season clothing collections. The designs I had to integrate were a challenge for me because of how abstract and unique they were. I\'m glad I was able to get the hang of it and learn new CSS techniques really quickly.',
+          text: 'As a Junior Web Developer for 2 Associés, I integrated a few landing pages for Laura Canada\'s season clothing collections. The designs I had to integrate were a challenge for me because of how abstract and unique they were. I\'m glad I was able to get the hang of it and learn new CSS techniques really quickly.',
           imageURL: 'laura-canada'
         },
         {
           title: 'YuCentrik',
           link: 'https://yucentrik.ca/en/',
-          text: 'As a Junior Web Developer for <a href="https://2associes.com">2 Associés</a>, I refactored a lot of the site\'s components. I also helped optimize the entire site to increase its performance score.',
+          text: 'As a Junior Web Developer for 2 Associés, I refactored a lot of the site\'s components. I also helped optimize the entire site to increase its performance score.',
           imageURL: 'yucentrik'
         },
         {
           title: '2 Associés',
           link: 'https://2associes.com/',
-          text: 'I\'ve worked with the web agency <a href="https://2associes.com">2 Associés</a> since 2016. During that time, I\'ve had the pleasure to work on their new website using the latest technologies and methodologies.',
+          text: 'I\'ve worked with the web agency 2 Associés since 2016. During that time, I\'ve had the pleasure to work on their new website using the latest technologies and methodologies.',
           imageURL: '2associes'
         }
       ]
@@ -100,6 +102,7 @@ export default {
     }
   },
   methods: {
+    doubleDigits: (number) => (`0${number}`).slice(-2),
     showNext: function () {
       if (this.current < this.count) this.current++
     },
