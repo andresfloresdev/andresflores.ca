9b25595486aeefb04140cb62911410df13bcf9a7
Andres Flores <andres@2associes.com>
2018-12-03 14:37:26 -0500

    Integrate CallToAction component

---
+++ components/CallToAction.vue
@@ -0,0 +1,18 @@
+<template>
+  <a :href="link" class="cta">
+    <slot></slot>
+  </a>
+</template>
+
+<script>
+
+export default {
+  props: {
+    link: {
+      type: String,
+      required: true
+    }
+  }
+}
+
+</script>
