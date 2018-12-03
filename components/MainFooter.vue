5f0f87eaa5d7588e9afcc2f7f8f74eef940c13c8
Andres Flores <andres@2associes.com>
2018-12-03 15:45:49 -0500

    Update MainFooter component markup and mobile styles

--- components/MainFooter.vue
+++ components/MainFooter.vue
@@ -15,13 +15,11 @@
           </call-to-action>
         </li>
       </ul>
-      <p class="main-footer-message">
-        {{ footerMessage }}
-      </p>
     </div>
     <div class="main-footer-copyright">
       <div class="main-footer-copyright-container">
         <p class="main-footer-copyright-text">
+          <span class="main-footer-copyright-message">{{ footerMessage }}</span>
           &copy; 2016 &mdash; {{ currentYear }}, Andrés Flores, Montréal (Québec), Canada.
         </p>
       </div>
