d34b27116199a1ae81a3a5f7067da75219ae121e
Andres Flores <andres@2associes.com>
2018-12-03 14:42:03 -0500

    Integrate MainHeader component

--- components/MainHeader.vue
+++ components/MainHeader.vue
@@ -1,20 +1,33 @@
 <template>
-  <div class="main-header-wrap">
+  <header class="main-header">
     <div class="main-header-container">
-      <brand class="main-header-logo"></brand>
-      <navigation class-prefix="main-header"></navigation>
+      <brand class="main-header-brand"></brand>
+      <h1 class="main-header-heading">
+        Andres Flores&nbsp;
+        <span class="main-header-subheading">Web consultant &amp; developer</span>
+      </h1>
+      <p class="main-header-text">
+        I strive to propel the harmony between design and technology forward with creative solutions.
+      </p>
+      <ul class="main-header-actions">
+        <li class="main-header-action">
+          <call-to-action link="mailto:info@andresflores.ca" class="main-header-cta">
+            Get in touch
+          </call-to-action>
+        </li>
+      </ul>
     </div>
-  </div>
+  </header>
 </template>
 
 <script>
 import Brand from '@/components/Brand'
-import Navigation from '@/components/Navigation'
+import CallToAction from '@/components/CallToAction'
 
 export default {
   components: {
     Brand,
-    Navigation
+    CallToAction
   }
 }
 </script>
