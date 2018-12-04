4bd396cab7effd138979ab14606ee1e7301144d5
Andres Flores <andres@2associes.com>
2018-12-04 16:06:11 -0500

    Update index page with new components and new content

--- pages/index.vue
+++ pages/index.vue
@@ -1,45 +1,36 @@
 <template>
   <div class="home-layout">
-    <!-- <hero></hero> -->
     <about></about>
-    <!-- <clients></clients> -->
-    <!-- <work></work> -->
-    <!-- <skills></skills> -->
+    <projects></projects>
   </div>
 </template>
 
 <script>
-import Hero from '@/components/Hero'
 import About from '@/components/About'
-import Clients from '@/components/Clients'
-import Work from '@/components/Work'
-import Skills from '@/components/Skills'
+import Projects from '@/components/Projects'
 
 export default {
   components: {
-    Hero,
     About,
-    Clients,
-    Work,
-    Skills
+    Projects
   },
   head () {
     return {
-      title: 'Andrés Flores, Freelance Web Developer',
+      title: 'Andrés Flores, Web Consultant & Developer',
       meta: [
         // Twitter Card
         { name:'twitter:card', content:'summary'},
         { name:'twitter:creator', content:'@andresfloresdev'},
         { name:'twitter:url', content:'https://andresflores.ca/'},
-        { name:'twitter:title', content:'Andrés Flores, Freelance Web Developer'},
-        { name:'twitter:description', content:'Freelance Web Developer based in Montréal, Canada.'},
+        { name:'twitter:title', content:'Andrés Flores, Web Consultant & Developer'},
+        { name:'twitter:description', content:'Web consultant & developer based in Montréal, Canada.'},
         { name:'twitter:image', content:'https://andresflores.ca/images/dark-bg-1200x.png'},
         // Facebook Open Graph
         { property: 'og:url', content: 'https://andresflores.ca/'},
         { property: 'og:type', content: 'website'},
-        { property: 'og:title', content: 'Andrés Flores, Freelance Web Developer'},
+        { property: 'og:title', content: 'Andrés Flores, Web Consultant & Developer'},
         { property: 'og:image', content: 'https://andresflores.ca/images/dark-bg-1200x.png'},
-        { property: 'og:description', content: 'Freelance Web Developer based in Montréal, Canada.'},
+        { property: 'og:description', content: 'Web consultant & developer based in Montréal, Canada.'},
         { property: 'og:locale', content: 'en-CA'}
       ]
     }
