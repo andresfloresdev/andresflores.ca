1a70d5a1fd591bc0d1b7f5dbc193df93dd775035
Andres Flores <andres@2associes.com>
2018-12-04 12:52:52 -0500

    Change socials background when hovering social link

--- components/About.vue
+++ components/About.vue
@@ -1,12 +1,15 @@
 <template>
   <section class="about">
+    <h2 class="about-heading">
+      About me
+    </h2>
     <div class="about-text-wrap">
       <p class="about-text">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
       </p>
     </div>
-    <div class="about-socials-wrap">
-      <socials class="about-socials"></socials>
+    <div class="about-socials-wrap" :style="{ backgroundColor: socialColor }">
+      <socials class="about-socials" @social-color="changeColor"></socials>
     </div>
   </section>
 </template>
@@ -17,6 +20,16 @@ import Socials from '@/components/Socials'
 export default {
   components: {
     Socials
+  },
+  data() {
+    return {
+      socialColor: '#427fed'
+    }
+  },
+  methods: {
+    changeColor(color) {
+      this.socialColor = color
+    }
   }
 }
 </script>
--- components/Socials.vue
+++ components/Socials.vue
@@ -1,7 +1,7 @@
 <template>
   <ul class="socials-list">
     <li class="socials-item" v-for="social in socials" :key="social.name">
-      <a :href="social.link" class="socials-link" :title="social.name">
+      <a :href="social.link" class="socials-link" :title="social.name" @mouseover.stop="$emit('social-color', social.color)" @mouseout="$emit('social-color', '#427fed')">
         <i class="fab fa-fw socials-icon" :class="social.icon"></i>
         <span class="socials-text">{{ social.name }}</span>
       </a>
@@ -17,22 +17,26 @@ export default {
         {
           name: 'Twitter',
           link: 'https://twitter.com/andresfloresdev',
-          icon: 'fa-twitter'
+          icon: 'fa-twitter',
+          color: '#1da1f2'
         },
         {
           name: 'Instagram',
           link: 'https://www.instagram.com/andresfloresdev/',
-          icon: 'fa-instagram'
+          icon: 'fa-instagram',
+          color: '#e1306c'
         },
         {
           name: 'LinkedIn',
           link: 'https://www.linkedin.com/in/andr%C3%A9s-flores-64538ba2/',
-          icon: 'fa-linkedin-in'
+          icon: 'fa-linkedin-in',
+          color: '#283e4a'
         },
         {
           name: 'Github',
           link: 'https://github.com/andresfloresdev',
-          icon: 'fa-github'
+          icon: 'fa-github',
+          color: '#24292e'
         }
       ]
     }
