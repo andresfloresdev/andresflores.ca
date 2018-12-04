2cea9affeeb891f34ded6f8b10f895c5bc524f2f
Andres Flores <andres@2associes.com>
2018-12-04 16:06:34 -0500

    Integrate Project and Projects components

---
+++ components/Project.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="project" :style="{ backgroundImage: `url(projects/${project.image})`}">
+    <div class="project-content">
+      <h3 class="project-heading">
+        {{ project.name }}
+      </h3>
+      <ul class="project-tags">
+        <li class="project-tag" v-for="(tag, index) in project.tags" :key="index">{{ tag }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['project']
+}
+</script>
---
+++ components/Projects.vue
@@ -0,0 +1,58 @@
+<template>
+  <section class="projects">
+    <h2 class="projects-heading">
+      Featured Projects
+    </h2>
+    <ul class="projects-list">
+      <li class="projects-single" v-for="(project, index) in projects" :key="index">
+        <project :project="project"></project>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+import Project from '@/components/Project'
+
+export default {
+  components: {
+    Project
+  },
+  data() {
+    return {
+      projects: [
+        {
+          name: 'DavidsTea',
+          image: 'davidstea.jpg',
+          tags: [
+            'Development & integration'
+          ]
+        },
+        {
+          name: 'CNESST',
+          image: 'cnesst.jpg',
+          tags: [
+            'Development & integration',
+            'UX/UI'
+          ]
+        },
+        {
+          name: 'DuProprio',
+          image: 'duproprio.jpg',
+          tags: [
+            'Design',
+            'Motion graphics'
+          ]
+        },
+        {
+          name: 'Laura Canada',
+          image: 'laura.jpg',
+          tags: [
+            'Development & integration'
+          ]
+        }
+      ]
+    }
+  }
+}
+</script>
