3481e173b16054f1f4305906bcf99386c5dceca4
Andres Flores <andres@2associes.com>
2018-12-05 04:37:20 -0500

    Add 500 font weight

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,7 +41,7 @@ module.exports = {
       },
       {
         rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css?family=Roboto:400,700'
+        href: 'https://fonts.googleapis.com/css?family=Roboto:400,500,700'
       },
       {
         rel: 'icon',
