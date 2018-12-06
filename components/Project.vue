af691b338177acc508dd5e052b67b2497711b58e
Andres Flores <andres@2associes.com>
2018-12-05 19:46:18 -0500

    Lazyload Project background images

--- components/Project.vue
+++ components/Project.vue
@@ -1,18 +1,17 @@
 <template>
-  <component :is="tag" class="project" :style="{ backgroundImage: `url(projects/${project.image})`}">
+  <component :is="tag" class="project" :style="{ backgroundImage: `url(${imagePath})`}">
     <div class="project-content">
       <h3 class="project-heading">
         {{ project.name }}
       </h3>
       <ul class="project-tags">
-        <li class="project-tag" v-for="(tag, index) in project.tags" :key="index">{{ tag }}</li>
+        <li class="project-tag" v-for="(tag, index) in project.tags" :key="index + 1">{{ tag }}</li>
       </ul>
     </div>
   </component>
 </template>
 
 <script>
-// TODO: Try lazy-loading background after component has mounted. Show blurred image on page load
 export default {
   props: {
     project: {
@@ -23,6 +22,34 @@ export default {
       type: String,
       required: true,
       default: 'div'
+    }
+  },
+  mounted: function () {
+    if (this.project.image) this.swapImage(this.project.image)
+  },
+  data() {
+    return {
+      imagePath: `projects/blurred-${this.project.image}`
+    }
+  },
+  methods: {
+    loadImage: function (url) {
+      return new Promise((resolve, reject) => {
+        let img = new Image()
+
+        img.addEventListener('load', e => resolve(img))
+        img.addEventListener('error', () => {
+          reject(new Error(`Failed to load image's URL: ${url}`))
+        })
+        img.src = url
+      })
+    },
+    swapImage: function (img) {
+      if (img) {
+        this.loadImage(`/projects/${img}`)
+          .then(img => this.imagePath = img.src)
+          .catch(error => console.log(error))
+      }
     }
   }
 }
