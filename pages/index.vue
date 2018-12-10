39acb1363dcf22339289b7bba2479de77688e153
Andres Flores <andres@2associes.com>
2018-12-10 13:12:20 -0500

    Fix MailchimpSubscribe ending tag

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
     <projects></projects>
     <!-- TODO: Integrate i18n -->
     <!-- TODO: Get mailchimp subscribe form to work -->
-    <!-- <mailchimp-subscribe url="https://andresflores.us19.list-manage.com/subscribe/post?u=ea080f01339525dc0f57cfdac&amp;id=7f4e27f502"></subscribe> -->
+    <!-- <mailchimp-subscribe url="https://andresflores.us19.list-manage.com/subscribe/post?u=ea080f01339525dc0f57cfdac&amp;id=7f4e27f502"></mailchimp-subscribe> -->
   </div>
 </template>
 
