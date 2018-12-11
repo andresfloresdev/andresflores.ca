1401581eb88cf57604fb01c5b3e4412661dae36c
Andres Flores <andres@2associes.com>
2018-12-11 16:06:15 -0500

    Integrate LangSwitcher component

---
+++ components/LangSwitcher.vue
@@ -0,0 +1,16 @@
+<template>
+  <nuxt-link :to="route" class="lang-switcher" v-html="locale"></nuxt-link>
+</template>
+
+<script>
+export default {
+  computed: {
+    locale: function () {
+      return (this.$i18n.locale === 'fr' ? 'en' : 'fr')
+    },
+    route: function () {
+      return (this.$i18n.locale === 'fr' ? '/' : '/fr')
+    },
+  }
+}
+</script>
