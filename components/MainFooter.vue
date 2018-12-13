79c8bc88f563d4a7e5416c5aaf63eb988d3c940e
Andres Flores <andres@2associes.com>
2018-12-13 15:59:44 -0500

    Hide McSubscribe component

--- components/MainFooter.vue
+++ components/MainFooter.vue
@@ -14,7 +14,7 @@
       </ul>
     </div>
     <!-- TODO: Integrate mailchimp form style-->
-    <mailchimp-subscribe url="https://andresflores.us19.list-manage.com/subscribe/post?u=ea080f01339525dc0f57cfdac&amp;id=7f4e27f502"></mailchimp-subscribe>
+    <!-- <mailchimp-subscribe url="https://andresflores.us19.list-manage.com/subscribe/post?u=ea080f01339525dc0f57cfdac&amp;id=7f4e27f502"></mailchimp-subscribe> -->
     <p class="main-footer-copyright" v-scroll-reveal="{ delay: scrollRevealDelay(3) }">
       &copy;&nbsp;2016&nbsp;&mdash;&nbsp;{{ currentYear }}, Andrés Flores, Montréal (Québec), Canada.
       {{ $t('madeWith') }} <a href="https://nuxtjs.org/" class="main-footer-copyright-made-with">Nuxt</a>.
