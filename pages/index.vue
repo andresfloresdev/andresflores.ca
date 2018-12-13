3a4070bd28f9872355428f2e5aa37c68cdea16d3
Andres Flores <andres@2associes.com>
2018-12-13 15:10:48 -0500

    Move MCSubscribe component to footer

--- pages/index.vue
+++ pages/index.vue
@@ -2,21 +2,17 @@
   <div class="home-layout">
     <about></about>
     <projects></projects>
-    <!-- TODO: Integrate mailchimp form style-->
-    <!-- <mailchimp-subscribe url="https://andresflores.us19.list-manage.com/subscribe/post?u=ea080f01339525dc0f57cfdac&amp;id=7f4e27f502"></mailchimp-subscribe> -->
   </div>
 </template>
 
 <script>
 import About from '@/components/About'
 import Projects from '@/components/Projects'
-import MailchimpSubscribe from '@/components/MailchimpSubscribe'
 
 export default {
   components: {
     About,
-    Projects,
-    MailchimpSubscribe
+    Projects
   },
   i18n: {
     messages: {
