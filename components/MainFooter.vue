4bd506aea92f0ca81aac3c34fad38f78c9b70d21
Andres Flores <andres@2associes.com>
2018-12-13 16:36:45 -0500

    Add GA events to new copyright links

--- components/MainFooter.vue
+++ components/MainFooter.vue
@@ -18,8 +18,8 @@
     <!-- <mailchimp-subscribe url="https://andresflores.us19.list-manage.com/subscribe/post?u=ea080f01339525dc0f57cfdac&amp;id=7f4e27f502"></mailchimp-subscribe> -->
     <p class="main-footer-copyright" v-scroll-reveal="{ delay: scrollRevealDelay(3) }">
       &copy;&nbsp;2016&nbsp;&mdash;&nbsp;{{ currentYear }}, Andrés Flores, Montréal (Québec), Canada.
-      {{ $t('madeWith') }} <a href="https://nuxtjs.org/" class="main-footer-copyright-made-with">Nuxt</a>.
-      {{ $t('credit') }} <a href="https://unsplash.com/collections/3663654/andresfloresca/dffc625539d6d6ef421402bef392fcf3" class="main-footer-copyright-credit">Unsplash</a>.
+      {{ $t('madeWith') }} <a href="https://nuxtjs.org/" class="main-footer-copyright-made-with" @click="$ga.event('Link', 'click', 'copyright', 'Nuxt')">Nuxt</a>.
+      {{ $t('credit') }} <a href="https://unsplash.com/collections/3663654/andresfloresca/dffc625539d6d6ef421402bef392fcf3" class="main-footer-copyright-credit" @click="$ga.event('Link', 'click', 'copyright', 'Unsplash')">Unsplash</a>.
     </p>
   </footer>
 </template>
