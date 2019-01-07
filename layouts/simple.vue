190eb2c87246e73beb12f7b81fd0766dffaeeeb5
Andres Flores <andres@2associes.com>
2019-01-07 15:05:02 -0500

    [#1] Integrate "Simple" layout

---
+++ layouts/simple.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="simple-layout">
+    <header class="simple-layout-header">
+      <navigation class="simple-layout-navigation"></navigation>
+    </header>
+    <main class="simple-layout-main">
+      <nuxt/>
+    </main>
+    <main-footer class="simple-layout-footer"></main-footer>
+  </div>
+</template>
+
+<script>
+import Navigation from '@/components/Navigation'
+import MainFooter from '@/components/MainFooter'
+
+export default {
+  components: {
+    Navigation,
+    MainFooter
+  }
+}
+</script>
