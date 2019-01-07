8d5c76cf04683c511b93c467810a8c91e6635ea1
Andres Flores <andres@2associes.com>
2019-01-07 15:05:41 -0500

    [#1] Create file for single project page

---
+++ pages/work/_slug.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Hello Worlds
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'simple',
+  validate ({ params }) {
+    return true
+  }
+}
+</script>
+
+<style>
+
+</style>
